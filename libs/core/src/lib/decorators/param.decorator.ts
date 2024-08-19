42bfa4185afec6addf61b2157122d06c04c8ee2e
ghanizadev <jf.melo6@gmail.com>
2024-08-19 23:21:44 +0300

    Refactor params

---
+++ libs/core/src/lib/decorators/param.decorator.ts
@@ -0,0 +1,48 @@
+import { SERVICE_RPC_ARGS_TOKEN, SERVICE_SERVER_RPC_TOKEN } from '../constants';
+
+export const BodyParam = () => {
+  return (target: any, propertyKey: string, parameterIndex: number) => {
+    let metadata =
+      Reflect.getMetadata(SERVICE_SERVER_RPC_TOKEN + propertyKey, target) ?? {};
+
+    const args =
+      Reflect.getMetadata(SERVICE_RPC_ARGS_TOKEN, target, propertyKey) ?? {};
+    Reflect.defineMetadata(
+      SERVICE_RPC_ARGS_TOKEN,
+      { ...args, [parameterIndex]: { type: 'body' } },
+      target,
+      propertyKey
+    );
+    Reflect.defineMetadata(
+      SERVICE_SERVER_RPC_TOKEN + propertyKey,
+      metadata,
+      target
+    );
+  };
+};
+
+export const MetadataParam = () => {
+  return (target: any, propertyKey: string, parameterIndex: number) => {
+    const args =
+      Reflect.getMetadata(SERVICE_RPC_ARGS_TOKEN, target, propertyKey) ?? {};
+    Reflect.defineMetadata(
+      SERVICE_RPC_ARGS_TOKEN,
+      { ...args, [parameterIndex]: { type: 'metadata' } },
+      target,
+      propertyKey
+    );
+  };
+};
+
+export const StreamParam = () => {
+  return (target: any, propertyKey: string, parameterIndex: number) => {
+    const args =
+      Reflect.getMetadata(SERVICE_RPC_ARGS_TOKEN, target, propertyKey) ?? {};
+    Reflect.defineMetadata(
+      SERVICE_RPC_ARGS_TOKEN,
+      { ...args, [parameterIndex]: { type: 'stream' } },
+      target,
+      propertyKey
+    );
+  };
+};
